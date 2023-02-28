80e2a34d63b6d6940e6988a618af0fd1d653606b
Pieter Jan Coeckelbergh <pieterjan.coeckelbergh@euronav.com>
2023-02-28 11:52:46 +0100

    cleanup unit of work

--- CarApp.Infrastructure/UnitOfWork.cs
+++ CarApp.Infrastructure/UnitOfWork.cs
@@ -11,21 +11,10 @@ namespace CarApp.Infrastructure
         public UnitOfWork(DbContextOptions<CarContext> options)
         {
             db = new CarContext(options);
+            CarRepository = new CarRepository(db);
         }
 
-        private ICarRepository _carRepository;
-
-        public ICarRepository CarRepository
-        {
-            get
-            {
-                if (_carRepository == null)
-                {
-                    _carRepository = new CarRepository(db);
-                }
-                return _carRepository;
-            }
-        }
+        public ICarRepository CarRepository { get; private init; }
 
         public Task<int> SaveChangesAsync(CancellationToken cancellationToken)
             => db.SaveChangesAsync(cancellationToken);
